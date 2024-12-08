61966cf895f6429ac95378e62c902a333f49858b
Beleguic <thibault.beleguic2@gmail.com>
2024-12-08 09:14:46 +0100

    Add Some Change

--- src/2021/day9/index.php
+++ src/2021/day9/index.php
@@ -6,29 +6,68 @@ echo("<pre>");
 $filename = './input';
 
 $file = fopen($filename, 'r');
-$safeReport = 0;
+$heightLocation = [];
 while ($line = fgets($file)) {
     $line = str_replace("\n", "", $line);
-
-
-
+    $heightLocation[] = str_split($line);
 }
 
 fclose($file);
 
-var_dump("---- Partie 1 ----");
+$levelHeightTotal = 0;
 
+foreach ($heightLocation as $line => $heightLine) {
+	foreach ($heightLine as $column => $height) {
+		$isLow = true;
+		if(isset($heightLocation[$line - 1][$column])){
+			if($height >= $heightLocation[$line - 1][$column]){
+				$isLow = false;
+			}
+		}
+		if(isset($heightLocation[$line + 1][$column])){
+			if($height >= $heightLocation[$line + 1][$column]){
+				$isLow = false;
+			}
+		}
+		if(isset($heightLocation[$line][$column - 1])){
+			if($height >= $heightLocation[$line][$column - 1]){
+				$isLow = false;
+			}
+		}
+		if(isset($heightLocation[$line][$column + 1])){
+			if($height >= $heightLocation[$line][$column + 1]){
+				$isLow = false;
+			}
+		}
+		if($isLow){
+			$levelHeightTotal += ($height + 1);
+		}
+	}
+}
+
+var_dump("---- Partie 1 ----");
+var_dump("Level Low : " . $levelHeightTotal);
 /*** Part 2 ***/
 
 $file = fopen($filename, 'r');
-$safeReport = 0;
+$heightLocation = [];
 while ($line = fgets($file)) {
     $line = str_replace("\n", "", $line);
-
-
-
+    $heightLocation[] = str_split($line);
 }
 
 fclose($file);
+
+foreach ($heightLocation as $line => $heightLine) {
+	foreach ($heightLine as $column => $height) {
+		if($height == 9){
+			echo("<span style='background-color:red;'>".$height."</span>");
+		}
+		else{
+			echo("<span>".$height."</span>");
+		}
+	}
+	echo("<br>");
+}
 
 var_dump("---- Partie 2 ----");
