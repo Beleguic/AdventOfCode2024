3374606a0fe19b847b5bef0282a2ec79d29b134b
Beleguic <thibault.beleguic2@gmail.com>
2024-12-09 07:17:05 +0100

    add day 9 One Star 2024

--- src/day7/index.php
+++ src/day7/index.php
@@ -6,29 +6,92 @@ echo("<pre>");
 $filename = './input';
 
 $file = fopen($filename, 'r');
-$safeReport = 0;
+$calibrationList = [];
 while ($line = fgets($file)) {
     $line = str_replace("\n", "", $line);
-
-
-
+    $calibrationTemp = explode(': ', $line);
+    $calibrationCalcul = explode(' ', $calibrationTemp[1]);
+    $calibrationList[] = ['result' => $calibrationTemp[0], 'numberOfCalcul' => $calibrationCalcul];
 }
 
 fclose($file);
+$trueCalibration = 0;
+foreach($calibrationList as $calibrationNumber => $calibrationData){
+
+    $solutions = [];
+
+    foreach($calibrationData['numberOfCalcul'] as $value){
+        $solutionTemp = [];
+        if(sizeof($solutions) == 0){
+            array_push($solutionTemp, $value);
+        }
+        else{
+            foreach($solutions as $key => $solution){
+                array_push($solutionTemp, $solution * $value);
+                array_push($solutionTemp, $solution + $value);
+            }
+        }
+
+        $solutions = $solutionTemp;
+
+    }
+
+    $resultAlreadyDone = false;
+    foreach($solutions as $value){
+        if($value == $calibrationData['result'] && !$resultAlreadyDone){
+            $resultAlreadyDone = true;
+            $trueCalibration += $calibrationData['result'];
+        }
+    }
+
+}
 
 var_dump("---- Partie 1 ----");
+var_dump("Number of Good Calibration : " . $trueCalibration);
 
 /*** Part 2 ***/
 
 $file = fopen($filename, 'r');
-$safeReport = 0;
+$calibrationList = [];
 while ($line = fgets($file)) {
     $line = str_replace("\n", "", $line);
-
-
-
+    $calibrationTemp = explode(': ', $line);
+    $calibrationCalcul = explode(' ', $calibrationTemp[1]);
+    $calibrationList[] = ['result' => $calibrationTemp[0], 'numberOfCalcul' => $calibrationCalcul];
 }
 
 fclose($file);
+$trueCalibration = 0;
+foreach($calibrationList as $calibrationNumber => $calibrationData){
+
+    $solutions = [];
+
+    foreach($calibrationData['numberOfCalcul'] as $value){
+        $solutionTemp = [];
+        if(sizeof($solutions) == 0){
+            array_push($solutionTemp, $value);
+        }
+        else{
+            foreach($solutions as $key => $solution){
+                array_push($solutionTemp, $solution * $value);
+                array_push($solutionTemp, $solution + $value);
+                array_push($solutionTemp, $solution . $value);
+            }
+        }
+        
+        $solutions = $solutionTemp;
+
+    }
+
+    $resultAlreadyDone = false;
+    foreach($solutions as $value){
+        if($value == $calibrationData['result'] && !$resultAlreadyDone){
+            $resultAlreadyDone = true;
+            $trueCalibration += $calibrationData['result'];
+        }
+    }
+
+}
 
 var_dump("---- Partie 2 ----");
+var_dump("Number of Good Calibration : " . $trueCalibration);
--- src/day9/index.php
+++ src/day9/index.php
@@ -1,22 +1,78 @@
 <?php
 
+set_time_limit(250);
+
 /*** Part 1 ***/
 echo("<pre>");
 
 $filename = './input';
 
 $file = fopen($filename, 'r');
-$safeReport = 0;
+$diskPart = [];
 while ($line = fgets($file)) {
     $line = str_replace("\n", "", $line);
-
-
-
+    $diskPart = str_split($line);
 }
 
 fclose($file);
 
+$diskPartRearrange = [];
+
+$files = true;
+$cpt = 0;
+foreach($diskPart as $key => $value){
+    if($files){
+        for($i = 0; $i < $value; $i++){
+            $diskPartRearrange[] = $cpt;
+        }
+        $cpt++;
+        $files = false;
+    }
+    else{
+        for($i = 0; $i < $value; $i++){
+            $diskPartRearrange[] = '.';
+        }
+        $files = true;
+    }
+}
+
+foreach($diskPartRearrange as $key => $value){
+    
+    if($value == '.'){
+        $lastPosition = defineLastPositionOfArray($diskPartRearrange);
+
+        $diskPartRearrange = swap($diskPartRearrange, $key, $lastPosition);
+        if($diskPartRearrange[$lastPosition] != '.'){
+
+            var_dump($lastPosition);
+            var_dump($key);
+
+            exit;
+        }
+
+    }
+}
+
+$done = true;
+for($i = 0; $i < sizeof($diskPartRearrange); $i++){
+    if($diskPartRearrange[$i] == '.' && $done){
+        $lastPosition = defineLastPositionOfArray($diskPartRearrange);
+        $diskPartRearrange = swap($diskPartRearrange, $i, $lastPosition);
+        $done = false;
+    }
+}
+
+$checksum = 0;
+foreach($diskPartRearrange as $key => $value){
+    if($value != '.'){
+        $checksum += ((int)$key * (int) $value);
+    }
+}
+
+var_dump($diskPartRearrange);
+
 var_dump("---- Partie 1 ----");
+var_dump('Checksum : ' . $checksum);
 
 /*** Part 2 ***/
 
@@ -32,3 +88,24 @@ while ($line = fgets($file)) {
 fclose($file);
 
 var_dump("---- Partie 2 ----");
+
+function swap($array, $key1, $key2) {
+    $temp = $array[$key1];
+    $array[$key1] = $array[$key2];
+    $array[$key2] = $temp;
+    return $array;
+}
+
+function defineLastPositionOfArray($array){
+
+    $totalKey = sizeof($array) - 1;
+    $tableau2 = array_reverse($array);
+    foreach($tableau2 as $key => $value){
+        if($value != '.'){
+            return $totalKey - $key;
+        }
+    }
+
+    return $totalKey;
+
+}
