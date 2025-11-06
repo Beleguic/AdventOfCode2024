f680a1ff8c9a9f0b9b53908a84767df091f74740
beleguic <thibault.beleguic2@gmail.com>
2025-11-06 11:04:08 +0100

    day 5 2016

--- src/2016/day5/index.php
+++ src/2016/day5/index.php
@@ -1,7 +1,7 @@
 <?php
 
 /*** Part 1 ***/
-echo("<pre>");
+/*echo("<pre>");
 
 $filename = './input';
 
@@ -20,7 +20,7 @@ var_dump("---- Partie 1 ----");
 
 /*** Part 2 ***/
 
-$file = fopen($filename, 'r');
+/*$file = fopen($filename, 'r');
 $safeReport = 0;
 while ($line = fgets($file)) {
     $line = str_replace("\n", "", $line);
@@ -32,3 +32,63 @@ while ($line = fgets($file)) {
 fclose($file);
 
 var_dump("---- Partie 2 ----");
+
+*/
+
+/*** Part 1 ***/
+
+echo("<pre>");
+
+$idDoor = "ugkcyxxp";
+//$idDoor = "abc";
+$index = 0;
+
+$password = "";
+
+while (strlen($password) < 8){
+    $md5tohash = $idDoor.$index;
+    $md5 = md5($md5tohash);
+
+    $zero = substr($md5, 0,5);
+    $passwordPart = substr($md5, 5,1);
+
+    if($zero === "00000"){
+        $password .= $passwordPart;
+    }
+
+    $index++;
+}
+
+
+var_dump("---- Partie 1 ----");
+var_dump($password);
+
+/*** Part 2 ***/
+
+$index = 0;
+$passwordFiled = true;
+$password = "________";
+
+while ($passwordFiled){
+    $md5tohash = $idDoor.$index;
+    $md5 = md5($md5tohash);
+
+    $zero = substr($md5, 0,5);
+    $passwordindex = substr($md5, 5,1);
+    $passwordPart = substr($md5, 6,1);
+
+    if($zero === "00000" && $passwordindex >= 0 && $passwordindex < 8){
+        if($password[$passwordindex] == "_"){
+            $password[$passwordindex] = $passwordPart;
+        }
+    }
+
+    if(!str_contains($password, '_')){
+        $passwordFiled = false;
+    }
+
+    $index++;
+}
+
+var_dump("---- Partie 2 ----");
+var_dump($password);
