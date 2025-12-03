9fb032ab8f285c7352b4ddc71a7c85653a5b86ea
beleguic <thibault.beleguic2@gmail.com>
2025-12-03 17:53:49 +0100

    Day 1,2,3 2020

--- src/2020/day1/index.php
+++ src/2020/day1/index.php
@@ -6,29 +6,59 @@ echo("<pre>");
 $filename = './input';
 
 $file = fopen($filename, 'r');
-$safeReport = 0;
+$numbers = [];
 while ($line = fgets($file)) {
     $line = str_replace("\n", "", $line);
-
-
-
+    $numbers[] = $line;
 }
 
 fclose($file);
 
+$answer = 0;
+$find = false;
+foreach ($numbers as $key1 => $value1) {
+    foreach ($numbers as $key2 => $value2) {
+        if(!$find){
+            if($value2 != $value1){
+                if($value1 + $value2 == 2020){
+                    $find = true;
+                    $answer = $value1 * $value2;
+                }
+            }
+        }
+    }
+}
+
 var_dump("---- Partie 1 ----");
+var_dump($answer);
 
 /*** Part 2 ***/
 
 $file = fopen($filename, 'r');
-$safeReport = 0;
+$numbers = [];
 while ($line = fgets($file)) {
     $line = str_replace("\n", "", $line);
-
-
-
+    $numbers[] = $line;
 }
 
 fclose($file);
 
+$answer = 0;
+$find = false;
+foreach ($numbers as $key1 => $value1) {
+    foreach ($numbers as $key2 => $value2) {
+        foreach ($numbers as $key3 => $value3) {
+            if(!$find){
+                if($value1 != $value2 && $value2 != $value3 && $value1 != $value3){
+                    if($value1 + $value2 +$value3 == 2020){
+                        $find = true;
+                        $answer = $value1 * $value2 * $value3;
+                    }
+                }
+            }
+        }
+    }
+}
+
 var_dump("---- Partie 2 ----");
+var_dump($answer);
--- src/2020/day3/index.php
+++ src/2020/day3/index.php
@@ -6,29 +6,98 @@ echo("<pre>");
 $filename = './input';
 
 $file = fopen($filename, 'r');
-$safeReport = 0;
+$map = [];
 while ($line = fgets($file)) {
     $line = str_replace("\n", "", $line);
-
-
-
+    $map[] = str_split($line);
 }
 
 fclose($file);
 
+$startPositionX = 0;
+$startPositionY = 0;
+
+$positionX = 1;
+$positionY = 3;
+
+$bottomOfMap = sizeof($map) - 1;
+$rightOfMap = sizeof($map[0]) -1;
+
+$numberOfTree = 0;
+
+while($startPositionX < $bottomOfMap){
+
+    $startPositionX += $positionX;
+    $startPositionY += $positionY;
+
+    if($startPositionY > $rightOfMap){
+        $startPositionY -= ($rightOfMap + 1);
+    }
+
+    $element = $map[$startPositionX][$startPositionY];
+
+    if($element == '#'){
+        $numberOfTree++;
+    }
+}
+
 var_dump("---- Partie 1 ----");
+var_dump($numberOfTree);
 
 /*** Part 2 ***/
 
 $file = fopen($filename, 'r');
-$safeReport = 0;
+$map = [];
 while ($line = fgets($file)) {
     $line = str_replace("\n", "", $line);
-
-
-
+    $map[] = str_split($line);
 }
 
 fclose($file);
 
 var_dump("---- Partie 2 ----");
+
+$scenario = [0 => ['x' => 1, 'y' => 1], 1 => ['x' => 1, 'y' => 3],2 => ['x' => 1, 'y' => 5],3 => ['x' => 1, 'y' => 7],4 => ['x' => 2, 'y' => 1],];
+
+$numberTotalOfTree = 0;
+
+foreach ($scenario as $key => $value) {
+
+    $startPositionX = 0;
+    $startPositionY = 0;
+
+    $positionX = $value['x'];
+    $positionY = $value['y'];
+
+    $bottomOfMap = sizeof($map) - 1;
+    $rightOfMap = sizeof($map[0]) -1;
+
+    $numberOfTree = 0;
+
+    while($startPositionX < $bottomOfMap){
+
+        $startPositionX += $positionX;
+        $startPositionY += $positionY;
+
+        if($startPositionY > $rightOfMap){
+            $startPositionY -= ($rightOfMap + 1);
+        }
+
+        $element = $map[$startPositionX][$startPositionY];
+
+        if($element == '#'){
+            $numberOfTree++;
+        }
+    }
+
+    var_dump("Scenario " . $key . " Right : " . $value['y'] . " Down : " . $value['x'] . " Numbre of tree : " . $numberOfTree);
+    if($numberTotalOfTree != 0){
+        $numberTotalOfTree = $numberTotalOfTree * $numberOfTree;
+    }
+    else{
+        $numberTotalOfTree = $numberOfTree;
+    }
+}
+
+
+var_dump($numberTotalOfTree);
