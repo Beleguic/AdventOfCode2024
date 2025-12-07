7cadf1af908ced2f085c6acd9016d9a0a98645f8
Beleguic <thibault.beleguic2@gmail.com>
2025-12-07 17:47:49 +0100

    day 7 2025

--- src/2025/day6/index.php
+++ src/2025/day6/index.php
@@ -6,29 +6,127 @@ echo("<pre>");
 $filename = './input';
 
 $file = fopen($filename, 'r');
-$safeReport = 0;
+$countGlobal = 0;
+$numbers = [];
+$sign = [];
 while ($line = fgets($file)) {
     $line = str_replace("\n", "", $line);
-
-
-
+    $line = trim($line);
+    $line = str_replace('      ', ' ',$line);
+    $line = str_replace('     ', ' ',$line);
+    $line = str_replace('    ', ' ',$line);
+    $line = str_replace('   ', ' ',$line);
+    $line = str_replace('  ', ' ',$line);
+    $line = explode(' ',$line);
+    if($line[0] == '+' || $line[0] == '*'){
+        foreach ($line as $key => $value) {
+            $sign[$key] = $value;
+        }
+    }
+    else{
+        foreach ($line as $key => $value) {
+            if(!isset($numbers[$key])){
+                $numbers[$key] = [];
+            }
+            $numbers[$key][] = $value;
+        }
+    } 
 }
 
 fclose($file);
 
+foreach ($numbers as $key => $value) {
+    $count = 0;
+    if($sign[$key] == '+'){
+        foreach ($value as $data) {
+            $count += $data;
+        }
+    }
+    elseif($sign[$key] == '*'){
+        foreach ($value as $data) {
+            if($count == 0){
+                $count = $data;
+            }
+            else{
+                $count *= $data;
+            }
+        }
+    }
+    $countGlobal += $count;
+}
+
 var_dump("---- Partie 1 ----");
+var_dump($countGlobal);
 
 /*** Part 2 ***/
 
 $file = fopen($filename, 'r');
-$safeReport = 0;
+$countGlobal = 0;
+$lines = [];
+$signLine = "";
 while ($line = fgets($file)) {
     $line = str_replace("\n", "", $line);
-
-
-
+    if($line[0] == '+' || $line[0] == '*'){
+        $signLine = $line;
+    }
+    else{
+        $lines[] = $line;
+    }
 }
 
 fclose($file);
 
+$signLine = str_split($signLine);
+$position = [];
+$index = 0;
+$length = 0;
+foreach ($signLine as $key => $value) {
+    if($value == '+' || $value == '*'){
+        $position[$index]['length'] = $length - 1;
+        $index++;
+        $position[$index]['start'] = $key;
+        $position[$index]['sign'] = $value;
+        $length = 0;
+    }
+    $length++;
+}
+$position[$index]['length'] = $length;
+unset($position[0]);
+$position = array_values($position);
+
+$numbers = [];
+foreach ($lines as $key => $value) {
+    $line = str_split($value);
+    foreach ($position as $positionKey => $positionData) {
+        for ($i=$positionData['start']; $i < $positionData['start'] + $positionData['length']; $i++) {
+            if(!isset($numbers[$positionKey][$i])){
+                $numbers[$positionKey][$i] = "";
+            }
+            $numbers[$positionKey][$i] .= $line[$i];
+        }
+    }
+}
+
+foreach ($numbers as $key => $value) {
+    $sign = $position[$key]['sign'];
+    $count = 0;
+    if($sign == '+'){
+        foreach ($value as $data) {
+            $count += $data;
+        }
+    }
+    elseif($sign == '*'){
+        foreach ($value as $data) {
+            if($count == 0){
+                $count = $data;
+            }
+            else{
+                $count *= $data;
+            }
+        }
+    }
+    $countGlobal += $count;
+}
+
 var_dump("---- Partie 2 ----");
+var_dump($countGlobal);
--- src/2025/day7/index.php
+++ src/2025/day7/index.php
@@ -6,29 +6,106 @@ echo("<pre>");
 $filename = './input';
 
 $file = fopen($filename, 'r');
-$safeReport = 0;
+$splitTime = 0;
+$previousLine = [];
+$index = 0;
 while ($line = fgets($file)) {
     $line = str_replace("\n", "", $line);
-
-
-
+    $line = str_split($line);
+    if($index == 0){
+        $previousLine = $line;
+    }
+    else{
+        $previousLineTemp = [];
+        //var_dump("@@@@@@@@@@@@@@@@@@@@" . $index);
+        foreach ($line  as $key => $value) {
+            if($value == '^' && $previousLine[$key] == '|'){
+                $previousLineTemp[$key] = '^';
+                $previousLineTemp[$key - 1] = '|';
+                $previousLineTemp[$key + 1] = '|';
+                $splitTime++;
+            }
+            elseif(($value == '.' && $previousLine[$key] == '|') || ($previousLine[$key] == 'S')){
+                $previousLineTemp[$key] = '|';
+            }
+            elseif(!isset($previousLineTemp[$key])){
+                $previousLineTemp[$key] = '.';
+            }
+            //var_dump($previousLineTemp);
+        }
+        var_dump(implode('', $previousLine));
+        $previousLine = $previousLineTemp;
+    }
+    $index++;
 }
 
 fclose($file);
 
 var_dump("---- Partie 1 ----");
+var_dump($splitTime);
+
 
 /*** Part 2 ***/
-
 $file = fopen($filename, 'r');
-$safeReport = 0;
+$splitTime = 0;
+$previousLine = [];
+$index = 0;
+$count = [];
 while ($line = fgets($file)) {
     $line = str_replace("\n", "", $line);
-
-
-
+    $line = str_split($line);
+    if($index == 0){
+        $previousLine = $line;
+        foreach ($line as $key => $value) {
+            if($value == 'S'){
+                $count[$index][$key] = 1;
+                break;
+            }
+        }
+    }
+    else{
+        $previousLineTemp = [];
+        //var_dump("@@@@@@@@@@@@@@@@@@@@" . $index);
+        foreach ($line  as $key => $value) {
+            if($value == '^' && $previousLine[$key] == '|'){
+                $previousLineTemp[$key] = '^';
+                $previousLineTemp[$key - 1] = '|';
+                $previousLineTemp[$key + 1] = '|';
+                $splitTime++;
+                if(!isset($count[$index][$key - 1])){
+                    $count[$index][$key - 1] = 0;
+                }
+                $count[$index][$key - 1] += $count[$index - 1][$key];
+                if(!isset($count[$index][$key + 1])){
+                    $count[$index][$key + 1] = 0;
+                }
+                $count[$index][$key + 1] += $count[$index - 1][$key];
+                
+            }
+            elseif(($value == '.' && $previousLine[$key] == '|') || ($previousLine[$key] == 'S')){
+                $previousLineTemp[$key] = '|';
+                if(!isset($count[$index][$key])){
+                    $count[$index][$key] = 0;
+                }
+                $count[$index][$key] += $count[$index - 1][$key];
+            }
+            elseif(!isset($previousLineTemp[$key])){
+                $previousLineTemp[$key] = '.';
+            }
+            //var_dump($previousLineTemp);
+        }
+        $previousLine = $previousLineTemp;
+    }
+    $index++;
 }
 
 fclose($file);
 
+
 var_dump("---- Partie 2 ----");
+$countTotal = 0;
+foreach (end($count) as $key => $value) {
+    $countTotal += $value;
+}
+var_dump($countTotal);
+exit;
