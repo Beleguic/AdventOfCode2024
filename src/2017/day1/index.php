1b5fd9bb0fe2f436f6113f6af623068d2837d9aa
beleguic <thibault.beleguic2@gmail.com>
2025-12-21 13:12:59 +0100

    Day 1 2017

--- src/2017/day1/index.php
+++ src/2017/day1/index.php
@@ -6,29 +6,53 @@ echo("<pre>");
 $filename = './input';
 
 $file = fopen($filename, 'r');
-$safeReport = 0;
+$sum = 0;
+$input = [];
 while ($line = fgets($file)) {
     $line = str_replace("\n", "", $line);
-
-
-
+    $input = str_split($line);
+    $input[sizeof($input)] = $input[0];
 }
 
 fclose($file);
 
+foreach ($input as $key => $value) {
+    if(isset($input[$key + 1]) && $value == $input[$key + 1]){
+        $sum += $value;
+    }
+}
+
 var_dump("---- Partie 1 ----");
+var_dump($sum);
+
 
 /*** Part 2 ***/
 
 $file = fopen($filename, 'r');
-$safeReport = 0;
+$sum = 0;
+$input = [];
 while ($line = fgets($file)) {
     $line = str_replace("\n", "", $line);
-
-
-
+    $input = str_split($line);
 }
+
+$firsthalf = array_slice($input, 0, intval(sizeof($input) / 2));
+$secondhalf = array_slice($input, intval(sizeof($input) / 2));
 
 fclose($file);
 
+foreach ($firsthalf as $key => $value) {
+    if(isset($secondhalf[$key]) && $value == $secondhalf[$key]){
+        $sum += $value;
+    }
+}
+
+foreach ($secondhalf as $key => $value) {
+    if(isset($firsthalf[$key]) && $value == $firsthalf[$key]){
+        $sum += $value;
+    }
+}
+
+
 var_dump("---- Partie 2 ----");
+var_dump($sum);
